6c9fe918db21f4e9b632c8c743f6a9722cb7eb14
Ebberth <ebberth@gmal.com>
2015-05-10 18:35:47 -0300

    Exercício Curso 4

---
+++ plot2.R
@@ -0,0 +1,29 @@
+# Project1 coursera Exploratory Data analysis.
+# Ploting histogram of Global Active Power over two days 2007-02-01
+# and 2007-02-02 of the data
+# "Individual household electric power consumption Data Set" from the
+# machine learning repository http://archive.ics.uci.edu/ml/
+
+
+# Download data.
+if (!file.exists("household_power_consumption.txt")){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                "household_power_consumption.zip")
+  unzip("household_power_consumption.zip")
+}
+
+
+# Read data and adjust time/date
+read_tb <- function() {
+  arq<<- read.table("household_power_consumption.txt",sep=";",dec=".", header=1, na.strings="?")
+  subArq <<- subset(arq,subset=(arq$Date=="1/2/2007"|arq$Date=="2/2/2007"))
+  subArq[[1]] <<- as.POSIXct(x = paste(as.Date(subArq[[1]], format = "%d/%m/%Y"),subArq[[2]]))
+}
+if (!exists("subArq") | (nrow(subArq)!= 2880)) read_tb()
+
+# Plot histogram of Global Active Power (kilowatts)
+
+png("plot2.png", width=480, height=480)
+plot(subArq[[1]],subArq[[3]],type="l",ylab='Global Active Power (kilowatts)',xlab="")
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,30 @@
+# Project1 coursera Exploratory Data analysis.
+# Ploting histogram of Global Active Power over two days 2007-02-01
+# and 2007-02-02 of the data
+# "Individual household electric power consumption Data Set" from the
+# machine learning repository http://archive.ics.uci.edu/ml/
+
+
+# Download data.
+if (!file.exists("household_power_consumption.txt")){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                "household_power_consumption.zip")
+  unzip("household_power_consumption.zip")
+}
+
+
+# Read data and adjust time/date
+read_tb <- function() {
+  arq<<- read.table("household_power_consumption.txt",sep=";",dec=".", header=1, na.strings="?")
+  subArq <<- subset(arq,subset=(arq$Date=="1/2/2007"|arq$Date=="2/2/2007"))
+  subArq[[1]] <<- as.POSIXct(x = paste(as.Date(subArq[[1]], format = "%d/%m/%Y"),subArq[[2]]))
+}
+if (!exists("subArq") | (nrow(subArq)!= 2880)) read_tb()
+
+# Plot histogram of Energy sub metering
+png("plot3.png", width=480, height=480)
+plot(subArq[[1]],subArq[[7]],type="l",ylab='Energy sub metering',xlab="")
+lines(subArq[[1]],subArq[[8]],col="red")
+lines(subArq[[1]],subArq[[9]],col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.7, lty=1, col=c("black","red","blue"))
+dev.off()
